7bbe103d2797e1be4d52ba2d149f87c6ea3c0d94
CubensisEchos <jordanreardon@hotmail.com>
2026-07-04 22:01:05 -0230

    Queue package and pom file added

---
+++ src/main/java/com/keyin/queue/Patient.java
@@ -0,0 +1,62 @@
+package com.keyin.queue;
+
+public class Patient
+{
+    private long patientId;
+    private String name;
+    private String reasonForVisit;
+
+    public Patient()
+    {
+
+    }
+
+    public Patient(String name, String reasonForVisit)
+    {
+        this.name = name;
+        this.reasonForVisit = reasonForVisit;
+    }
+
+    public Patient(long patientId, String name, String reasonForVisit)
+    {
+        this.patientId = patientId;
+        this.name = name;
+        this.reasonForVisit = reasonForVisit;
+    }
+
+    public long getPatientId()
+    {
+        return patientId;
+    }
+
+    public void setPatientId(long patientId)
+    {
+        this.patientId = patientId;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getReasonForVisit()
+    {
+        return reasonForVisit;
+    }
+
+    public void setReasonForVisit(String reasonForVisit)
+    {
+        this.reasonForVisit = reasonForVisit;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Patient ID: " + patientId + ", Name: " + name + ", Reason for Visit: " + reasonForVisit;
+    }
+}
